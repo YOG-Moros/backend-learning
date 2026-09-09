8578c91c6bc92105dcc18d69be1f27179e0aae7e
YOG-Moros <2081524274@qq.com>
2026-09-09 19:22:43 +0900

    在 02-config 模块，创建包、配置文件、启动主类等，使用 @Value 注解来单个读取 application.yml 中的配置项内容，包括官方配置和自定义配置，编写接口输出

--- 02-config/src/test/java/com/example/config/properties/DevelopmentProfilePropertiesTest.java
+++ 02-config/src/test/java/com/example/config/properties/DevelopmentProfilePropertiesTest.java
@@ -1,13 +1,7 @@
 package com.example.config.properties;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import java.time.Duration;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("dev")
